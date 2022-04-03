a3029fa0e6a83309a288a79368195ba17343a85f
Océane Renoux <oceane.renoux@hotmail.fr>
2022-04-03 22:08:44 +0200

    [ADD] Like Remark

--- public/js/like.js
+++ public/js/like.js
@@ -28,3 +28,34 @@ $('.unlike_post').on('click',function(){
         }
     });
 })
+
+$('.like_remark').on('click',function(){
+    console.log($(this).data('url'));
+    $.ajax({
+        url: $(this).data('url'),
+        type: "GET",
+        success: function (data) {
+            if (data) {
+                console.log(data)
+                $('.id-'+data).attr('data-url',"{{ path('unlike_remark', {'id': post.id }) }}");
+                $('.id-'+data).removeClass('far fa-heart like_remark').addClass('unlike_remark fas fa-heart')
+            }
+        }
+    });
+
+})
+
+
+$('.unlike_remark').on('click',function(){
+    console.log("bla")
+    $.ajax({
+        url: $(this).data('url'),
+        type: "GET",
+        success: function (data) {
+            if (data) {
+                $('.id-'+data).attr('data-url',"{{ path('like_remark', {'id': post.id }) }}");
+                $('.id-'+data).removeClass('fas fa-heart unlike_remark').addClass('like_remark far fa-heart')
+            }
+        }
+    });
+})
