e14644666c7c76f114aae808966bcf20eff28adc
NilsMillot <compte.mxs@hotmail.fr>
2022-07-06 15:24:05 +0200

    [FIX] name class of challengeuserregisterfixture

--- src/DataFixtures/ChallengeUserRegisterFixtures.php
+++ src/DataFixtures/ChallengeUserRegisterFixtures.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ChallengesUserRegisterFixtures extends Fixture implements DependentFixtureInterface
+class ChallengeUserRegisterFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
