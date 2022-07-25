f5803df45dbb2b5d532dfacb22177d0c463188eb
Océane Renoux <oceane.renoux@hotmail.fr>
2022-07-26 00:17:00 +0200

    [ADD] Page stat challenge + category (ui)

--- public/js/statistical.js
+++ public/js/statistical.js
@@ -1,69 +1,97 @@
 $(document).ready(function () {
-  $.ajax({
-    url: "/admin/statistical/data",
-    type: "GET",
-    success: function (data) {
-      console.log(data);
-      if (data) {
-        let category = $("#categories");
 
-        let categGraph = new Chart(category, {
-          type: "pie",
-          data: {
-            labels: data.categoryNom,
-            datasets: [
-              {
-                label: "Repartition des catégories",
-                data: data.categoryCount,
-                backgroundColor: [
-                  "#" +
-                    (
-                      "000000" +
-                      Math.floor(Math.random() * 16777215).toString(16)
-                    ).slice(-6),
-                  "#" +
-                    (
-                      "000000" +
-                      Math.floor(Math.random() * 16777215).toString(16)
-                    ).slice(-6),
-                  "#" +
-                    (
-                      "000000" +
-                      Math.floor(Math.random() * 16777215).toString(16)
-                    ).slice(-6),
-                ],
-              },
-            ],
-          },
-        });
-        let challengeDate = $("#challengeDate");
-        let challengeDateGraph = new Chart(challengeDate, {
-          type: "line",
-          data: {
-            labels: data.challengeDate,
-            datasets: [
-              {
-                label: "Nombre de challenge par Date",
-                data: data.challengeCount,
-              },
-            ],
-          },
-          options: {
-            scales: {
-              xAxes: [
-                {
-                  type: "time",
-                  time: {
-                    displayFormats: {
-                      quarter: "MMM YYYY",
+    /**
+     * Partie Challenges
+     */
+    $.ajax({
+        url: "/admin/statistical/data/challenge",
+        type: "GET",
+        success: function (data) {
+            if (data) {
+                let challengeDate = $("#challengeDate");
+                let challengeDateGraph = new Chart(challengeDate, {
+                    type: "line",
+                    data: {
+                        labels: data.challengeDate,
+                        datasets: [
+                            {
+                                label: "Nombre de challenge par Date",
+                                data: data.challengeCount,
+                                backgroundColor:'rgba(0,0,0,0)',
+                                borderColor:'#00acc1',
+                                borderWidth:1,
+                            },
+                        ],
                     },
-                  },
-                },
-              ],
-            },
-          },
-        });
-      }
-    },
-  });
+                    options:{
+                        scales: {
+                            y: {
+                                title: {
+                                    display: true,
+                                },
+                                min: 0,
+                                max: 20,
+                                ticks: {
+                                    // forces step size to be 50 units
+                                    stepSize: 5
+                                }
+                            }
+                        },
+                        legend:{
+                            labels:{
+                                fontColor:'#fff',
+                            }
+                        }
+                    }
+                });
+            }
+        },
+    });
+
+    /**
+     * Partie Category
+     */
+    $.ajax({
+        url: "/admin/statistical/data/category",
+        type: "GET",
+        success: function (data) {
+            if (data) {
+                let category = $("#categories");
+                let categGraph = new Chart(category, {
+                    type: "doughnut",
+                    data: {
+                        labels: data.categoryNom,
+                        datasets: [
+                            {
+                                label: "Repartition des catégories",
+                                data: data.categoryCount,
+                                backgroundColor: [
+                                    "#" +
+                                    (
+                                        "000000" +
+                                        Math.floor(Math.random() * 16777215).toString(16)
+                                    ).slice(-6),
+                                    "#" +
+                                    (
+                                        "000000" +
+                                        Math.floor(Math.random() * 16777215).toString(16)
+                                    ).slice(-6),
+                                    "#" +
+                                    (
+                                        "000000" +
+                                        Math.floor(Math.random() * 16777215).toString(16)
+                                    ).slice(-6),
+                                ],
+                            },
+                        ],
+                    },
+                    options: {
+                        responsive: false
+                    }
+                });
+            }
+        }
+    });
 });
+
+
--- src/Controller/Back/StatisticalController.php
+++ src/Controller/Back/StatisticalController.php
@@ -17,20 +17,9 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/admin/statistical')]
 class StatisticalController extends AbstractController
 {
-    #[Route('/data', name: 'admin_statistical_data', methods: ['GET'])]
-    public function data(CategoryRepository $categoryRepository, ChallengesRepository $challengesRepository): JsonResponse
+    #[Route('/data/challenge', name: 'admin_statistical_data_challenge', methods: ['GET'])]
+    public function dataChallenge( ChallengesRepository $challengesRepository): JsonResponse
     {
-
-        // On va chercher les données de la table category
-        $category = $categoryRepository->findAll();
-
-        // On démonte les données pour les afficher dans le tableau et les donners à chart.js
-        $categoryNom = [];
-        $categoryCount = [];
-        foreach ($category as $cat) {
-            $categoryNom[] = $cat->getName();
-            $categoryCount[] = count($cat->getChallenges());
-        }
 
         // On va chercher le nombre de challenges par date
         $challenges = $challengesRepository->countByDate();
@@ -44,21 +33,72 @@ class StatisticalController extends AbstractController
 
         return new JsonResponse(
             [
-                'categoryNom' => $categoryNom,
-                'categoryCount' => $categoryCount,
                 'challengeDate' => $challengeDate,
                 'challengeCount' => $challengesCount
             ]
         );
-
     }
 
-    #[Route('/', name: 'admin_statistical_index', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/challenge', name: 'admin_statistical_challenge', methods: ['GET'])]
+    public function indexChallenge(ChallengesRepository $challengesRepository): Response
     {
-        return $this->render('back/statistical/index.html.twig', [
-            'title' => 'statistique',
+        $challenges = $challengesRepository->findAll();
+        $countLike = 0;
+        foreach ($challenges as $challenge)
+        {
+            foreach ($challenge->getUserLikeChallenges() as $like){
+                $countLike ++;
+            }
+        }
+
+        return $this->render('back/statistical/challenges.html.twig', [
+            'countLike' => $countLike,
+            'challengeTotaux' => count($challenges),
+            'title' => 'Statistique Challenge',
         ]);
     }
+
+    #[Route('/data/category', name: 'admin_statistical_data_category', methods: ['GET'])]
+    public function dataCategory(CategoryRepository $categoryRepository): JsonResponse
+    {
+        // On va chercher les données de la table category
+        $category = $categoryRepository->findAll();
+
+        // On démonte les données pour les afficher dans le tableau et les donners à chart.js
+        $categoryNom = [];
+        $categoryCount = [];
+        foreach ($category as $cat) {
+            $categoryNom[] = $cat->getName();
+            $categoryCount[] = count($cat->getChallenges());
+        }
+
+        return new JsonResponse(
+            [
+                'categoryNom' => $categoryNom,
+                'categoryCount' => $categoryCount,
+            ]
+        );
+    }
+
+    #[Route('/category', name: 'admin_statistical_category', methods: ['GET'])]
+    public function indexCategory(CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findAll();
+//        $countLike = 0;
+//        foreach ($challenges as $challenge)
+//        {
+//            foreach ($challenge->getUserLikeChallenges() as $like){
+//                $countLike ++;
+//            }
+//        }
+
+        return $this->render('back/statistical/category.html.twig', [
+//            'countLike' => $countLike,
+            'categoryTotaux' => count($category),
+            'title' => 'Statistique Categories',
+        ]);
+    }
+
+
 
 }
