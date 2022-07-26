011f3c349e830b96a004e74cb0fc794b7b0b8011
Océane Renoux <oceane.renoux@hotmail.fr>
2022-07-26 17:59:05 +0200

    [ADD] Home page for backOffice

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -49,6 +49,43 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $query->orderBy('dateUser', 'ASC');
         return $query->getQuery()->getResult();
     }
+
+    public function findRecentUser()
+    {
+        $query = $this->createQueryBuilder('u');
+        $query->select('u.username');
+        $query->orderBy('u.createdAt', 'ASC');
+        $query->setMaxResults(1);
+        return $query->getQuery()->getResult();
+    }
+
+//    public function findByMaxChallenge()
+//    {
+//        $query = $this->createQueryBuilder('u');
+//        $query->select('u.username, COUNT(u.username)');
+//        $query->join('u.challenge', 'cu');
+//        $query->groupBy('u.username');
+//        $query->having('count(u.username)');
+//        $query->select('max(countuser) as usercount');
+//        $query->select('u.username, COUNT(u.username)');
+//        $query->join('u.challenge', 'cus');
+//        $query->groupBy('u.username');
+//
+//
+//
+//        return $query->getQuery()->getResult();
+//
+//    }
+
+//SELECT username, COUNT(username)
+//FROM "user"   join challenges_user on "user".id = challenges_user.user_id GROUP BY username
+//HAVING COUNT (username)=(
+//SELECT MAX(countuser)
+//FROM (
+//SELECT username, COUNT(username) countuser
+//FROM "user" join challenges_user on "user".id = challenges_user.user_id
+//GROUP BY username) as usercount);
+
     // public function findOrCreateFromOauth (GithubResourceOwner $resourceOwner)
     // {
     //     $user = $this->createQueryBuilder('u')
