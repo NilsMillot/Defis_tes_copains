c9ef73cc34e87160ea354cb847901b66b6e6d805
Océane Renoux <oceane.renoux@hotmail.fr>
2022-07-28 10:06:25 +0200

    [ADD] Fixe like challenge

--- public/js/like.js
+++ public/js/like.js
@@ -1,28 +1,26 @@
 $('.liked').on('click',function(){
     let count = $(this).prev().text();
-    if($(this).hasClass('like') ){
+    if($(this).hasClass('like-challenge') ){
         $.ajax({
             url: $(this).data('url'),
             type: "GET",
             success: function (data) {
                 if (data) {
-                    $('.like-id-'+data).removeClass('far fa-heart like').addClass('unlike fas fa-heart')
+                    $('.like-challenge-id-'+data).removeClass('far fa-heart like-challenge').addClass('unlike-challenge fas fa-heart')
                     $('.id-'+data+'-count').text(Number(count)+1)
                 }
             }
         });
-    }else{
+    }else if($(this).hasClass('unlike-challenge') ){
         $.ajax({
             url: $(this).data('url'),
             type: "GET",
             success: function (data) {
                 if (data) {
-                    $('.like-id-'+data).removeClass('fas fa-heart unlike').addClass('like far fa-heart')
+                    $('.like-challenge-id-'+data).removeClass('fas fa-heart unlike-challenge').addClass('like-challenge far fa-heart')
                     $('.id-'+data+'-count').text(Number(count)-1)
                 }
             }
         });
     }
-
-
 })
